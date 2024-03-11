ea60a0af9f9a3481def4f992e71ff971b898ac10
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-03-11 10:08:11 +0530

    converted to ts and es modules and added ts-node

---
+++ src/errors/index.ts
@@ -0,0 +1,11 @@
+import CustomAPIError from './custom-api'
+import UnauthenticatedError from './unauthenticated'
+import NotFoundError from './not-found'
+import BadRequestError from './bad-request'
+
+export  {
+  CustomAPIError,
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+}
