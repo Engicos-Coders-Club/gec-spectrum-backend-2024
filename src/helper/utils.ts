f96b8ef051cc3238c876722b6b1b7525ce0214c0
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-03-17 23:51:27 +0530

    optimised getDepartmentNameById, removed redundant function and loop for json object

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -1,46 +1,24 @@
-    const departments = [
-        {
-            "_id": "65eed7ed2378ad4bb65db272",
-            "name": "Information Technology Engineering",
-            "__v": 0
-        },
-        {
-            "_id": "65eed7fb2378ad4bb65db274",
-            "name": "Computer Engineering",
-            "__v": 0
-        },
-        {
-            "_id": "65eed8182378ad4bb65db276",
-            "name": "Electronics & Telecommunications Engineering",
-            "__v": 0
-        },
-        {
-            "_id": "65eed8352378ad4bb65db278",
-            "name": "Electrical & Electronics Engineering",
-            "__v": 0
-        },
-        {
-            "_id": "65eed83e2378ad4bb65db27a",
-            "name": "Mechanical Engineering",
-            "__v": 0
-        },
-        {
-            "_id": "65eed8442378ad4bb65db27c",
-            "name": "Civil Engineering",
-            "__v": 0
-        },
-        {
-            "_id": "65eed84d2378ad4bb65db27e",
-            "name": "VLSI Engineering",
-            "__v": 0
-        }
-    ]
-// Function to retrieve the department name by _id
+    const departments: {[key:string]:string} = 
+    {
+        
+            "65eed7ed2378ad4bb65db272" : "Information Technology Engineering",
+ 
+            "65eed7fb2378ad4bb65db274" : "Computer Engineering",
+ 
+            "65eed8182378ad4bb65db276" : "Electronics & Telecommunications Engineering",
+ 
+            "65eed8352378ad4bb65db278" : "Electrical & Electronics Engineering",
+ 
+            "65eed83e2378ad4bb65db27a" : "Mechanical Engineering",
+ 
+            "65eed8442378ad4bb65db27c": "Civil Engineering",
+ 
+            "65eed84d2378ad4bb65db27e": "VLSI Engineering",
+    }
+        
+
+    // Function to retrieve the department name by _id
 export function getDepartmentNameById(id:string) {
-    let department = ""
-    departments.forEach((item) => {
-        if(item._id === id)
-            department = item.name
-    })
-    return department;
+    return departments[id];
 }
+
